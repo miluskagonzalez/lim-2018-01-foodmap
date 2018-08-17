9f9f8a487ea6a96c27afd0f139205e86f83283a1
spacedgatito <miluska.gonzalezar@gmail.com>
2018-08-16 23:02:34 -0500

    Completa filtrado

---
+++ src/js/database.js
@@ -0,0 +1,68 @@
+const restaurants = [
+  {
+    id: 'llsc',
+    name: 'La Lucha Sangucheria Criolla',
+    picture: '../assets/images/llsc-pic.png',
+    description: 'Sánguches y jugos',
+    local: { 
+      address: 'Av. Malecón de la Reserva 610 Interior 231, 2do Piso, Miraflores',
+      map: '../assets/images/llsc-map.png'
+    },
+    tags: [ 
+      'sandwiches', 'sánguches', 'sanguches', 'sanguich', 'sangüich', 'jugos', 'café', 'cafe', 'asado de res', 
+      'chicharrón', 'chicharron', 'jamón del pais', 'jamon del pais', 'jamon y huevos', 'jamón y huevos', 
+      'pavo a la leña', 'pollo a la leña', 'jamon serrano', 'jamón serrano', 'pollo con piña', 'hamburguesas', 
+      'lomo fino', 'club sandwich', 'chicha', 'papas fritas huayro', 'comida rápida', 'fast food', 'comida local', 
+      'comida para llevar', 'desayuno', 'almuerzo', 'cena', 'brunch'
+    ]
+  },
+  {
+    id: 'bigdb',
+    name: 'BLUE: il gelato del barrio',
+    picture: '../assets/images/bigdb-pic.jpg',
+    description: 'Helados artesanales',
+    local: { 
+      address: 'Jiron 28 de julio 202, Barranco', 
+      map: '../assets/images/bigdb-map.png'
+    },
+    tags: [ 
+      'gelato artesanal', 'gelatos artesanales', 'helado artesanal', 'helados artesanales', 'helado vegano', 
+      'helados veganos', 'helado vegetariano', 'helados vegetarianos', 'opciones veganas', 'heladería', 'postres', 
+      'vegan ice cream', 'opciones vegetarianas', 'café', 'cafe', 'expresso', 'frutas'
+    ]
+  },
+  {
+    id: 'ayhsc',
+    name: 'Ayahuasca Resto Bar',
+    picture: '../assets/images/ayhsc-pic.jpg',
+    description: 'Dinner & drinks',
+    local: { 
+      address: 'Pronlongación San Martin 130, Barranco', 
+      map: '../assets/images/ayhsc-map.png'
+    },
+    tags: [
+      'chancho al cilindro', 'brochetas', 'pollo a la parrilla', 'pancita', 'choncholi', 'picarones', 'lomo', 'cerdo',
+      'champiñones', 'piqueos', 'pisco sour', 'tequeños', 'langostinos', 'portobellos', 'pastel de choclo', 'cangrejo',
+      'conchas a la parmesana', 'chicharrón', 'chicharrón', 'sánguches', 'sanguches', 'sandwiches', 'sangüich', 'makis',
+      'carpaccio', 'tiradito', 'alitas', 'butifarra', 'hamburguesas', 'salchipapas', 'pizzeta', 'pulpo', 'pulpito', 
+      'sushi', 'causa', 'peruana', 'latina', 'bar', 'pub', 'sudamericana', 'comida marina', 'mariscos'
+    ]
+  },
+  {
+    id: 'cntrn',
+    name: 'Canta Rana',
+    picture: '../assets/images/cntrn-pic.jpg',
+    description: 'Comida peruana',
+    local: { 
+      address: 'Calle Genova 101, Barranco', 
+      map: '../assets/images/cntrn-map.png'
+    },
+    tags: [ 
+      'comida peruana', 'comida latina', 'mariscos', 'sopas', 'opciones sin gluten', 'pescados', 'pulpito', 'pulpo',
+      'cevichería', 'ceviche mixto', 'comida marina', 'caldo de gallina', 'causa', 'arroz con mariscos', 'milanesa',
+      'conchas a la parmesana', 'ceviche de pescado', 'champiñones', 'chicharrón de pescado', 'langostinos', 'conchas',
+      'pulpo al olivo', 'conchas a la chorrillana', 'gnnochis', 'arroz chaufa', 'tiradito'
+    ]
+  },
+]
+  
--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,51 @@
-const 
+const searchRestaurant = document.getElementById('restaurant-search');
+const restaurantSection = document.getElementById('restaurant-section');
+
+const filterBySearch = (search) => restaurants.filter(({ name, tags }) => {
+  return name.toLowerCase().includes(search.toLowerCase()) || tags.toString().includes(search.toLowerCase())
+});
+
+const restaurantTemplate = restaurantList => restaurantList.map(({ id, name, picture, description }) => {
+  return `<div class="col s12 m6 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src=${picture}>
+          <span class="card-title bold-text"></span>
+          <button class="btn-floating halfway-fab waves-effect waves-light red modal-trigger" data-target="restaurant-modal">
+            <i id=${id} class="material-icons tea-rose">add</i>
+          </button>
+        </div>
+        <div class="card-content">
+          <p class="grey-text bold-text">${name}</p>
+          <p class="grey-text darken-5">${description}</p>
+        </div>
+      </div>
+    </div>`
+}).join('');
+
+const renderRestaurants = restaurantList => restaurantSection.innerHTML = restaurantTemplate(restaurantList);
+
+searchRestaurant.addEventListener('input', event => renderRestaurants(filterBySearch(event.target.value)));
+searchRestaurant.addEventListener('submit', (event) => event.preventDefault());
+
+restaurantSection.addEventListener('click', (event) => {
+  const { 
+    name, description, local
+  } = restaurants.find(restaurant => restaurant.id === event.target.id)
+  console.log(name, description)
+  document.getElementById('restaurant-modal').innerHTML = `<div class="modal-content"> 
+      <div class="row no-bottom-margin">
+        <a href="#" class="modal-close right">X</a>
+        <img class="col s10 m7 offset-s1" src="${local.map}">
+        <div class="col s12 m5">
+          <p class="card-title bold-text middle-blue-green-text hide-on-med-and-up">${name}</p>
+          <span class="hide-on-med-and-up grey-text">${local.address}</span>
+          <h5 class="hide-on-small-only bold-text middle-blue-green-text">${name}</h5>
+          <h6 class="hide-on-small-only tea-rose-text">${description}</h6>
+          <p class="hide-on-small-only grey-text">${local.address}</p>
+        </div>
+      </div>
+    </div>`
+})
+
+renderRestaurants(restaurants);
